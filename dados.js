40b2bd7f9fd19c811b309f400be77ddf14534619
Layo Brum <layobmedeiros@gmail.com>
2024-09-07 14:44:17 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -7,96 +7,87 @@ let dados = [
         jogo: "League of Legends"
     },
     {
-        titulo: "Sacy",
+        titulo: "<span>Sacy</span>",
         descricao: "Líder nato e estrategista brilhante, Sacy se tornou uma das figuras mais importantes do cenário competitivo de Valorant no Brasil. Com passagens por equipes como LOUD e Sentinels, ele conquistou diversos títulos e reconhecimento internacional. Sua visão de jogo e habilidade o tornaram um dos jogadores mais temidos do servidor.",
         link_liquipedia: "https://liquipedia.net/valorant/Sacy",
         jogo: "Valorant"
     },
     {
-        titulo: "FalleN",
+        titulo: "<span>FalleN</span>",
         descricao: "Lenda do Counter-Strike, FalleN é considerado um dos maiores jogadores de todos os tempos. Com uma carreira marcada por títulos importantes e momentos históricos, ele liderou a cena brasileira por anos. Sua inteligência tática e habilidade com o AWP o transformaram em um ícone do jogo.",
         link_liquipedia: "https://liquipedia.net/counterstrike/FalleN",
         jogo: "Counter Strike"
     },
     {
-        titulo: "coldzera",
+        titulo: "<span>coldzera</span>",
         descricao: "Outro grande nome do Counter Strike brasileiro, conhecido por suas jogadas incríveis.",
         jogo: "Counter Strike",
         link_liquipedia: "https://liquipedia.net/counterstrike/coldzera"
     },
     {
-        titulo: "Taco",
+        titulo: "<span>Taco</span>",
         descricao: "Veterano do Counter Strike e campeão de Majors.",
         jogo: "Counter Strike",
         link_liquipedia: "https://liquipedia.net/counterstrike/Taco"
     },
     {
-        titulo: "fer",
+        titulo: "<span>fer</span>",
         descricao: "Outro jogador experiente do Counter Strike com diversas conquistas.",
         jogo: "Counter Strike",
         link_liquipedia: "https://liquipedia.net/counterstrike/fer"
     },
     {
-        titulo: "boltz",
+        titulo: "<span>boltz</span>",
         descricao: "Jogador brasileiro com passagens por diversas equipes de destaque no Counter Strike.",
         jogo: "Counter Strike",
         link_liquipedia: "https://liquipedia.net/counterstrike/boltz"
     },
     {
-        titulo: "brTT",
+        titulo: "<span>brTT</span>",
         descricao: "Um dos principais jogadores de League of Legends do Brasil.",
         jogo: "League of Legends",
         link_liquipedia: "https://liquipedia.net/leagueoflegends/brTT"
     },
     {
-        titulo: "Nobru",
+        titulo: "<span>Nobru</span>",
         descricao: "Campeão mundial de Free Fire e um dos maiores streamers do Brasil.",
         jogo: "Free Fire",
         link_liquipedia: "https://liquipedia.net/freefire/Nobru"
     },
     {
-        titulo: "Aspas",
+        titulo: "<span>aspas</span>",
         descricao: "Campeão mundial de VALORANT com a LOUD e um dos jogadores mais talentosos do jogo.",
         jogo: "VALORANT",
         link_liquipedia: "https://liquipedia.net/valorant/Aspas"
     },
     {
-        titulo: "Psycho",
+        titulo: "<span>Psycho</span>",
         descricao: "Um dos jogadores de Rainbow Six Siege que mais faturou em premiações.",
         jogo: "Rainbow Six Siege",
         link_liquipedia: "https://liquipedia.net/rainbowsix/Psycho"
     },
     {
-        titulo: "KamiKaze",
+        titulo: "<span>Kamikaze</span>",
         descricao: "Outro grande nome do Rainbow Six Siege, atualmente na Ninjas in Pyjamas.",
         jogo: "Rainbow Six Siege",
         link_liquipedia: "https://liquipedia.net/rainbowsix/KamiKaze"
     },
     {
-        titulo: "Heat",
+        titulo: "<span>heat</span>",
         descricao: "Jogador brasileiro de VALORANT, conhecido por sua habilidade com operadores de defesa.",
         jogo: "VALORANT",
         link_liquipedia: "https://liquipedia.net/valorant/Heat"
     },
     {
-        titulo: "Pancada",
+        titulo: "<span>pANcada</span>",
         descricao: "Jogador brasileiro de VALORANT, conhecido por sua agressividade e jogadas imprevisíveis.",
         jogo: "VALORANT",
         link_liquipedia: "https://liquipedia.net/valorant/Pancada"
     },
     {
-        titulo: "Kami",
+        titulo: "<span>Kami</span>",
         descricao: "Um dos principais jogadores de League of Legends do Brasil, conhecido por sua habilidade com diversos campeões.",
         jogo: "League of Legends",
         link_liquipedia: "https://liquipedia.net/leagueoflegends/Kami"
     },
 ];
-// function adicionarSpanAoTitulo(dado.titulo) {
-//     return {
-//       ...dado.titulo,
-//       titulo: `<span>${dado.titulo}</span>`
-//     };
-//   }
-  
-//   // Aplicando a função a cada jogador
-//   const jogadoresComSpan = dados.map(adicionarSpanAoTitulo);
